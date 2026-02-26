14c10586ac5c47276d42c4be84e22f722f80cd9b
Caio Ribeiro <devcaioribeiro@gmail.com>
2026-02-26 09:25:06 -0300

    renaming file and fixing exports

--- function/images.js
+++ function/images.js
@@ -1,9 +1,9 @@
-async function onRequestGet(context) {
+export async function onRequestGet(context) {
     const response = await context.next();
     return response
 }
 
-async function onRequestGet(context) {
+export async function onRequestPost(context) {
     const response = await context.next();
     return response
 }
