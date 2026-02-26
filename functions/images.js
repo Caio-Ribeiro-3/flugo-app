e319ce815e2dc8636cbda6540bf6c5bc4e13a122
Caio Ribeiro <devcaioribeiro@gmail.com>
2026-02-26 09:38:56 -0300

    removing cloudflare functions and config file

--- functions/images.js
+++
@@ -1,9 +0,0 @@
-export async function onRequestGet(context) {
-    const response = await context.next();
-    return response
-}
-
-export async function onRequestPost(context) {
-    const response = await context.next();
-    return response
-}
